4690c907d663551dce26759f5c15c5e1e248293f
unknown <shaham298@gmail.com>
2023-03-17 16:00:46 +0200

    chnaged forms.py file

--- users/forms.py
+++ users/forms.py
@@ -3,3 +3,5 @@ from django.contrib.auth.forms import UserCreationForm
 class EmailedUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email','first_name', 'last_name')
+
+#This is a forms file
